d29c8cdf025ac46f7f5c8683e0382530b0fe5fc0
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2026-01-17 23:17:57 -0300

    [Aula11] Corrige bug CORS

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.areteacademy.com.br/api',
+        target: 'https://api.areteacademy.com.br',
         changeOrigin: true,
         secure: true,
       },
